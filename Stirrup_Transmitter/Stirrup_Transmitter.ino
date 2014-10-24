8744024d1227f998e17798dc443487346a547b39
ArchimedesPi <archimedespi3141@gmail.com>
2014-10-23 23:20:11 -0500

    Comment, stub program, license (on Transmitter)

---
+++ Stirrup_Transmitter/Stirrup_Transmitter.ino
@@ -0,0 +1,17 @@
+/* 
+  Stirrup Transmitter
+  for ɣ-FM (vf0.1) Hear Hear
+  
+  By Liam Marshall
+  
+  #################################
+  # This software is BSD licensed #
+  #################################
+*/
+
+void setup() {
+}
+
+void loop() {
+}
+
